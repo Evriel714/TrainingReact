96bc44416b11870cbda8d4effa25dc9a1acb1699
Evriel714 <evriel.leovaztion@gmail.com>
2023-01-19 12:19:58 +0700

    first commit

---
+++ code-training/src/test/Test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Test.css';
+
+function Test() {
+  return (
+    <div className="App">
+        <h1>Training IT div</h1>
+    </div>
+  );
+}
+
+export default Test;
